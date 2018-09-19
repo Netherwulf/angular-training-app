bd6be1f652bb85a8e36a4f5046882635165eeb91
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-19 23:26:57 +0200

    Added ngFor directive with index extraction
    Added ngFor directive with index extraction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showSecret = false;
+  logs = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.logs.push(new Date);
+  }
 }
