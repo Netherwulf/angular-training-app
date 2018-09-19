70e735c1f6e3e07379eefae77fff4ad9b598cbda
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-19 22:19:41 +0200

    Added structural directive
    Added structural directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestName';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
