668bdbca983377aac4568948874494e0af9e60ac
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-19 13:58:49 +0200

    Added different types of component selectors
    Added different types of component selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  selector: 'app-servers', // selector via element
+  // selector: '[app-servers]', // selector via attribute
+  // selector: '.app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
